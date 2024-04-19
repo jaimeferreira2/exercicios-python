517c701cc4caeb1f79329e7e10023202fdef2ea8
Jaime Ferreira <jaimeferreira2005@yahoo.com>
2024-04-19 20:26:08 -0300

    sla

--- cursoemvideo/desafio22.py
+++
@@ -1,5 +0,0 @@
-nome = str(input("Digite seu nome: ")).strip()
-
-print("analisando seu nome em maiusculo é: {}".format(nome.upper()))
-print("Analisando seu nome em minuscolo é: {} ".format(nome.lower()))
-print("Analisando seu nome ele tem {} caracteres".format(len(nome)-nome.count(" ")))
--- desafio22.py
+++ desafio22.py
@@ -1,3 +1,5 @@
-frase = ("aaaaaaaa")
+nome = str(input("Digite seu nome: ")).strip()
 
-len(frase)
+print("analisando seu nome em maiusculo é: {}".format(nome.upper()))
+print("Analisando seu nome em minuscolo é: {} ".format(nome.lower()))
+print("Analisando seu nome ele tem {} caracteres".format(len(nome)-nome.count(" ")))
